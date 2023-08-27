3301007f96abf6a47108a74f5128883b9ea3042a
aroldos91 <61445551+aroldos91@users.noreply.github.com>
2023-08-27 09:29:44 -0400

    Update Pagination.tsx

--- src/app/components/common/Pagination.tsx
+++ src/app/components/common/Pagination.tsx
@@ -1,182 +1,15 @@
-import React, { Component, Fragment } from 'react';
-import PropTypes from 'prop-types';
-import './../../../../styles/pagination.scss';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import "./../../../../styles/pagination.scss";
 
-class Pagination extends Component<any, any> {
+interface Params {
   pageLimit: any;
   totalRecords: any;
   pageNeighbours: any;
-  totalPages: any;
-  static propTypes: { totalRecords: PropTypes.Validator<any>; pageLimit: PropTypes.Requireable<any>; pageNeighbours: PropTypes.Requireable<any>; onPageChanged: PropTypes.Requireable<(...args: any[]) => any>; };
-  constructor(props: { totalRecords?: null | undefined; pageLimit?: 30 | undefined; pageNeighbours?: 0 | undefined; }) {
-    super(props);
-    const { totalRecords = null, pageLimit = 30, pageNeighbours = 0 } = props;
-
-    this.pageLimit = typeof pageLimit === 'number' ? pageLimit : 30;
-    this.totalRecords = typeof totalRecords === 'number' ? totalRecords : 0;
-
-    // pageNeighbours can be: 0, 1 or 2
-    this.pageNeighbours = typeof pageNeighbours === 'number'
-      ? Math.max(0, Math.min(pageNeighbours, 2))
-      : 0;
-
-    this.totalPages = Math.ceil(this.totalRecords / this.pageLimit);
-
-    this.state = { currentPage: 1 };
-    }
-
-    /**
-   * Let's say we have 10 pages and we set pageNeighbours to 2
-   * Given that the current page is 6
-   * The pagination control will look like the following:
-   *
-   * (1) < {4 5} [6] {7 8} > (10)
-   *
-   * (x) => terminal pages: first and last page(always visible)
-   * [x] => represents current page
-   * {...x} => represents page neighbours
-   */
-    fetchPageNumbers = () => {
-        const totalPages = this.totalPages;
-        const currentPage = this.state.currentPage;
-        const pageNeighbours = this.pageNeighbours;
-
-        /**
-         * totalNumbers: the total page numbers to show on the control
-         * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
-         */
-        const totalNumbers = (this.pageNeighbours * 2) + 3;
-        const totalBlocks = totalNumbers + 2;
-
-        if (totalPages > totalBlocks) {
-        const startPage = Math.max(2, currentPage - pageNeighbours);
-        const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
-        let pages = range(startPage, endPage);
-
-        /**
-         * hasLeftSpill: has hidden pages to the left
-         * hasRightSpill: has hidden pages to the right
-         * spillOffset: number of hidden pages either to the left or to the right
-         */
-        const hasLeftSpill = startPage > 2;
-        const hasRightSpill = (totalPages - endPage) > 1;
-        const spillOffset = totalNumbers - (pages.length + 1);
-
-        switch (true) {
-            // handle: (1) < {5 6} [7] {8 9} (10)
-            case (hasLeftSpill && !hasRightSpill): {
-            const extraPages = range(startPage - spillOffset, startPage - 1);
-            pages = [LEFT_PAGE, ...extraPages, ...pages];
-            break;
-            }
-
-            // handle: (1) {2 3} [4] {5 6} > (10)
-            case (!hasLeftSpill && hasRightSpill): {
-            const extraPages = range(endPage + 1, endPage + spillOffset);
-            pages = [...pages, ...extraPages, RIGHT_PAGE];
-            break;
-            }
-
-            // handle: (1) < {4 5} [6] {7 8} > (10)
-            case (hasLeftSpill && hasRightSpill):
-            default: {
-            pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
-            break;
-            }
-        }
-
-        return [1, ...pages, totalPages];
-        }
-
-        return range(1, totalPages);
-    }
-
-    render() {
-        if (!this.totalRecords || this.totalPages === 1) return null;
-    
-        const { currentPage } = this.state;
-        const pages = this.fetchPageNumbers();
-    
-        return (
-          <Fragment>
-            <nav aria-label="Pagination" className='flex justify-end items-center content-center p-2'>
-              <ul className="pagination inline-flex -space-x-px text-sm">
-                { pages.map((page, index) => {
-    
-                  if (page === LEFT_PAGE) return (
-                    <li key={index} className="page-item">
-                      <a className="page-link" href="#" aria-label="Previous" onClick={this.handleMoveLeft}>
-                        <span aria-hidden="true">&laquo;</span>
-                        <span className="sr-only">Previous</span>
-                      </a>
-                    </li>
-                  );
-    
-                  if (page === RIGHT_PAGE) return (
-                    <li key={index} className="page-item">
-                      <a className="page-link" href="#" aria-label="Next" onClick={this.handleMoveRight}>
-                        <span aria-hidden="true">&raquo;</span>
-                        <span className="sr-only">Next</span>
-                      </a>
-                    </li>
-                  );
-    
-                  return (
-                    <li key={index} className={`page-item${ currentPage === page ? ' active' : ''}`}>
-                      <a className="page-link" href="#" onClick={ this.handleClick(page) }>{ page }</a>
-                    </li>
-                  );
-    
-                }) }
-    
-              </ul>
-            </nav>
-          </Fragment>
-        );
-    }
-    
-    componentDidMount() {
-        this.gotoPage(1);
-      }
-    
-      gotoPage = (page: number) => {
-        const { onPageChanged = (f: any) => f } = this.props;
-        const currentPage = Math.max(0, Math.min(page, this.totalPages));
-        const paginationData = {
-          currentPage,
-          totalPages: this.totalPages,
-          pageLimit: this.pageLimit,
-          totalRecords: this.totalRecords
-        };
-    
-        this.setState({ currentPage }, () => onPageChanged(paginationData));
-      }
-    
-      handleClick = (page: any) => (evt: { preventDefault: () => void; }) => {
-        evt.preventDefault();
-        this.gotoPage(page);
-      }
-    
-      handleMoveLeft = (evt: { preventDefault: () => void; }) => {
-        evt.preventDefault();
-        this.gotoPage(this.state.currentPage - (this.pageNeighbours * 2) - 1);
-      }
-    
-      handleMoveRight = (evt: { preventDefault: () => void; }) => {
-        evt.preventDefault();
-        this.gotoPage(this.state.currentPage + (this.pageNeighbours * 2) + 1);
-      }
-    
+  page: any;
+  // totalPages: any;
+  onPageChanged: (value:any) => any;
 }
-
-Pagination.propTypes = {
-  totalRecords: PropTypes.number.isRequired,
-  pageLimit: PropTypes.number,
-  pageNeighbours: PropTypes.number,
-  onPageChanged: PropTypes.func
-};
-
-// ...
 
 const LEFT_PAGE = -1;
 const RIGHT_PAGE = -2;
@@ -195,6 +28,162 @@ const range = (from: number, to: number, step = 1) => {
   }
 
   return range;
-}
+};
+
+const Pagination = ({
+  pageLimit,
+  totalRecords,
+  pageNeighbours,
+  page,
+  onPageChanged,
+}: Params) => {
+  const currentPage = React.useMemo(() => {
+    return Math.ceil(page);
+  }, [page]);
+
+  const totalPages = React.useMemo(() => {
+    return Math.ceil((totalRecords ?? 1) / (pageLimit ?? 30));
+  }, [pageLimit, totalRecords]);
+
+  const fetchPageNumbers = () => {
+    /**
+     * totalNumbers: the total page numbers to show on the control
+     * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
+     */
+    const totalNumbers = (pageNeighbours ?? 0 * 2) + 3;
+    const totalBlocks = totalNumbers + 2;
+
+    if (totalPages > totalBlocks) {
+      const startPage = Math.max(2, currentPage - pageNeighbours);
+      const endPage = Math.min(totalPages - 1, currentPage + pageNeighbours);
+      let pages = range(startPage, endPage);
+
+      /**
+       * hasLeftSpill: has hidden pages to the left
+       * hasRightSpill: has hidden pages to the right
+       * spillOffset: number of hidden pages either to the left or to the right
+       */
+      const hasLeftSpill = startPage > 2;
+      const hasRightSpill = totalPages - endPage > 1;
+      const spillOffset = totalNumbers - (pages.length + 1);
+
+      switch (true) {
+        // handle: (1) < {5 6} [7] {8 9} (10)
+        case hasLeftSpill && !hasRightSpill: {
+          const extraPages = range(startPage - spillOffset, startPage - 1);
+          pages = [LEFT_PAGE, ...extraPages, ...pages];
+          break;
+        }
+
+        // handle: (1) {2 3} [4] {5 6} > (10)
+        case !hasLeftSpill && hasRightSpill: {
+          const extraPages = range(endPage + 1, endPage + spillOffset);
+          pages = [...pages, ...extraPages, RIGHT_PAGE];
+          break;
+        }
+
+        // handle: (1) < {4 5} [6] {7 8} > (10)
+        case hasLeftSpill && hasRightSpill:
+        default: {
+          pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
+          break;
+        }
+      }
+
+      return [1, ...pages, totalPages];
+    }
+
+    return range(1, totalPages);
+  };
+
+  const _pages = React.useMemo(() => {
+    return fetchPageNumbers();
+  }, [pageLimit, totalRecords]);
+
+  const gotoPage = (_page: number) => {
+    onPageChanged(_page);
+  }
+
+  const handleClick = (_page: any) => (evt: { preventDefault: () => void; }) => {
+    evt.preventDefault();
+    gotoPage(_page);
+  }
+
+  const handleMoveLeft = (evt: { preventDefault: () => void; }) => {
+    evt.preventDefault();
+    gotoPage(currentPage - (pageNeighbours * 2) - 1);
+  }
+
+  const handleMoveRight = (evt: { preventDefault: () => void; }) => {
+    evt.preventDefault();
+    gotoPage(currentPage + (pageNeighbours * 2) + 1);
+  }
+
+  return (
+    <>
+      {!totalRecords || !totalPages || totalPages === 1 ? (
+        <></>
+      ) : (
+        <>
+          <nav
+            aria-label="Pagination"
+            className="flex justify-end items-center content-center p-2"
+          >
+            <ul className="pagination inline-flex -space-x-px text-sm">
+              {_pages.map((page, index) => {
+                if (page === LEFT_PAGE)
+                  return (
+                    <li key={index} className="page-item">
+                      <a
+                        className="page-link"
+                        href="#"
+                        aria-label="Previous"
+                        onClick={handleMoveLeft}
+                      >
+                        <span aria-hidden="true">&laquo;</span>
+                        <span className="sr-only">Previous</span>
+                      </a>
+                    </li>
+                  );
+
+                if (page === RIGHT_PAGE)
+                  return (
+                    <li key={index} className="page-item">
+                      <a
+                        className="page-link"
+                        href="#"
+                        aria-label="Next"
+                        onClick={handleMoveRight}
+                      >
+                        <span aria-hidden="true">&raquo;</span>
+                        <span className="sr-only">Next</span>
+                      </a>
+                    </li>
+                  );
+
+                return (
+                  <li
+                    key={index}
+                    className={`page-item${
+                      currentPage === page ? " active" : ""
+                    }`}
+                  >
+                    <a
+                      className="page-link"
+                      href="#"
+                      onClick={handleClick(page)}
+                    >
+                      {page}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </>
+      )}
+    </>
+  );
+};
 
 export default Pagination;
