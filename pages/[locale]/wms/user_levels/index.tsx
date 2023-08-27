8fa6550548b48d23439c343943c5727c4a8f6d70
aroldos91 <61445551+aroldos91@users.noreply.github.com>
2023-08-27 09:54:37 -0400

    Update WMS LEVELS USER

--- pages/[locale]/wms/user_levels/index.tsx
+++ pages/[locale]/wms/user_levels/index.tsx
@@ -1,37 +1,20 @@
-import Head from 'next/head';
-import Layout from '../../../../src/app/layout';
-import ProtectedRoute from '../../../../src/app/components/common/ProtectedRoute';
-import { UserLevelListProps } from '../../../../src/types/user_levels';
-import { getUserLevels } from '../../../../src/services/api.user_level';
-import { GetServerSidePropsContext } from 'next';
-import UserLevelTable from '../../../../src/app/components/wms/userLevel/UserLevelTable';
-import { indexServices } from '../../../../src/services/api.services';
+import Head from "next/head";
+import Layout from "../../../../src/app/layout";
+import ProtectedRoute from "../../../../src/app/components/common/ProtectedRoute";
+import UserLevelTable from "../../../../src/app/components/wms/userLevel/UserLevelTable";
 
-const UserLevel = ({ userLevelList, servicesList }: UserLevelListProps) => {
-  
+const UserLevel = () => {
   return (
-  <ProtectedRoute>
-      <Layout>
-        <Head>
-          <title>Don Express Warehouse</title>
-          <link rel="icon" href="/icon_favicon.png" />
-        </Head>
-        <UserLevelTable userLevelList={userLevelList ? userLevelList : []} servicesList={servicesList} />
-      </Layout>
-    </ProtectedRoute>
-    );
+    <Layout>
+      <Head>
+        <title>Don Express Warehouse</title>
+        <link rel="icon" href="/icon_favicon.png" />
+      </Head>
+      <ProtectedRoute>
+        <UserLevelTable/>
+      </ProtectedRoute>
+    </Layout>
+  );
 };
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const userLevelList = await getUserLevels(context);
-  const servicesList = await indexServices(context);
-
-  return {
-    props: {
-        userLevelList,
-        servicesList
-    }
-  }
-}
 
 export default UserLevel;
