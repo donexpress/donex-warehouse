1b6c73fdd0cda6ca50deecdbafe2d9e1813ecec7
ergarcia <ergarcia@uci.ci>
2023-10-02 12:13:45 -0400

    config environment

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,23 +1,33 @@
 import { BASE_URL } from "../config/index";
 
+const getBaseUrl = () => {
+  console.log(process.env.WAREHOUSE_ENV)
+  console.log(process.env.WAREHOUSE_API_HOST)
+  if (process.env.WAREHOUSE_ENV && (process.env.WAREHOUSE_ENV === 'staging' || process.env.WAREHOUSE_ENV === 'prod')) {
+    console.log('entro')
+    return process.env.WAREHOUSE_API_HOST;
+  }
+  return BASE_URL;
+}
+
 export const loginPath = () => {
-  return BASE_URL + "/api/v1/login";
+  return getBaseUrl() + "/api/v1/login";
 };
 
 export const userPath = () => {
-  return BASE_URL + "/api/v1/user";
+  return getBaseUrl() + "/api/v1/user";
 };
 
 export const countUsersPath = () => {
-  return BASE_URL + "/api/v1/user/count";
+  return getBaseUrl() + "/api/v1/user/count";
 };
 
 export const removeUserPath = (id: number) => {
-  return `${BASE_URL}/api/v1/user/${id}`;
+  return `${getBaseUrl()}/api/v1/user/${id}`;
 };
 
 export const userStatePath = () => {
-  return BASE_URL + "/api/v1/user/states";
+  return getBaseUrl() + "/api/v1/user/states";
 };
 
 export const staffPath = (page?: number, rowsPerPage?: number) => {
@@ -25,55 +35,55 @@ export const staffPath = (page?: number, rowsPerPage?: number) => {
   if (page && rowsPerPage) {
     params = `?current_page=${page}&number_of_rows=${rowsPerPage}`;
   }
-  return BASE_URL + "/api/v1/staff" + params;
+  return getBaseUrl() + "/api/v1/staff" + params;
 };
 
 export const countStaffPath = () => {
-  return BASE_URL + "/api/v1/staff/count";
+  return getBaseUrl() + "/api/v1/staff/count";
 };
 
 export const removeStaffPath = (id: number) => {
-  return `${BASE_URL}/api/v1/staff/${id}`;
+  return `${getBaseUrl()}/api/v1/staff/${id}`;
 };
 
 export const subsidiaryPath = () => {
-  return BASE_URL + "/api/v1/subsidiary";
+  return getBaseUrl() + "/api/v1/subsidiary";
 };
 
 export const RegionalDivisionPath = () => {
-  return BASE_URL + "/api/v1/regional_division";
+  return getBaseUrl() + "/api/v1/regional_division";
 };
 
 export const WarehousePath = () => {
-  return BASE_URL + "/api/v1/cargo_station";
+  return getBaseUrl() + "/api/v1/cargo_station";
 };
 
 export const getStateWarehousePath = () => {
-  return BASE_URL + "/api/v1/cargo_station/states";
+  return getBaseUrl() + "/api/v1/cargo_station/states";
 };
 
 export const warehousePath = () => {
-  return BASE_URL + "/api/v1/cargo_station";
+  return getBaseUrl() + "/api/v1/cargo_station";
 };
 
 export const countWarehousePath = () => {
-  return BASE_URL + "/api/v1/cargo_station/count";
+  return getBaseUrl() + "/api/v1/cargo_station/count";
 };
 
 export const paymentMethodPath = () => {
-  return BASE_URL + "/api/v1/payment_method";
+  return getBaseUrl() + "/api/v1/payment_method";
 };
 
 export const countPaymentMethodPath = () => {
-  return BASE_URL + "/api/v1/payment_method/count";
+  return getBaseUrl() + "/api/v1/payment_method/count";
 };
 
 export const userLevelPath = () => {
-  return BASE_URL + "/api/v1/user_level";
+  return getBaseUrl() + "/api/v1/user_level";
 };
 
 export const countUserLevelPath = () => {
-  return BASE_URL + "/api/v1/user_level/count";
+  return getBaseUrl() + "/api/v1/user_level/count";
 };
 
 export const storagePlanPath = (status: string = '', page?: number, rowsPerPage?: number) => {
@@ -81,103 +91,103 @@ export const storagePlanPath = (status: string = '', page?: number, rowsPerPage?
   if (page && rowsPerPage) {
     params = `${ params === "" ? '?' : '&'}current_page=${page}&number_of_rows=${rowsPerPage}`;
   }
-  return BASE_URL + "/api/v1/storage_plan" + params;
+  return getBaseUrl() + "/api/v1/storage_plan" + params;
 };
 
 export const storagePlanStatePath = () => {
-  return BASE_URL + "/api/v1/storage_plan/states";
+  return getBaseUrl() + "/api/v1/storage_plan/states";
 };
 
 export const storagePlanCountPath = () => {
-  return BASE_URL + "/api/v1/storage_plan/count";
+  return getBaseUrl() + "/api/v1/storage_plan/count";
 };
 
 export const storagePlanByOrderNumberPath = (order_number: string) => {
   const params = `?query=${order_number}`;
-  return BASE_URL + "/api/v1/storage_plan" + params;
+  return getBaseUrl() + "/api/v1/storage_plan" + params;
 };
 
 export const countStoragePlanPath = () => {
-  return BASE_URL + "/api/v1/storage_plan/count";
+  return getBaseUrl() + "/api/v1/storage_plan/count";
 };
 
 export const selfPath = () => {
-  return BASE_URL + "/api/v1/self";
+  return getBaseUrl() + "/api/v1/self";
 };
 
 export const packingListPath = () => {
-  return BASE_URL + "/api/v1/packing_list";
+  return getBaseUrl() + "/api/v1/packing_list";
 };
 
 export const countPackingListPath = () => {
-  return BASE_URL + "/api/v1/packing_list/count";
+  return getBaseUrl() + "/api/v1/packing_list/count";
 };
 
 export const countriesPath = () => {
-  return BASE_URL + "/api/v1/countries";
+  return getBaseUrl() + "/api/v1/countries";
 };
 
 export const servicePath = () => {
-  return BASE_URL + "/api/v1/service";
+  return getBaseUrl() + "/api/v1/service";
 };
 
 export const countRolePath = () => {
-  return BASE_URL + "/api/v1/role/count";
+  return getBaseUrl() + "/api/v1/role/count";
 };
 
 export const rolePath = () => {
-  return BASE_URL + "/api/v1/role";
+  return getBaseUrl() + "/api/v1/role";
 };
 
 export const staffStatePath = () => {
-  return BASE_URL + "/api/v1/staff/states";
+  return getBaseUrl() + "/api/v1/staff/states";
 };
 
 export const organizationPath = () => {
-  return BASE_URL + "/api/v1/organization";
+  return getBaseUrl() + "/api/v1/organization";
 };
 
 export const countOrganizationPath = () => {
-  return BASE_URL + "/api/v1/organization/count";
+  return getBaseUrl() + "/api/v1/organization/count";
 };
 
 export const countServicePath = () => {
-  return BASE_URL + "/api/v1/service/count";
+  return getBaseUrl() + "/api/v1/service/count";
 };
 
 export const whPath = () => {
-  return BASE_URL + "/api/v1/aos_warehouse";
+  return getBaseUrl() + "/api/v1/aos_warehouse";
 };
 
 export const countWhPath = () => {
-  return BASE_URL + "/api/v1/aos_warehouse/count";
+  return getBaseUrl() + "/api/v1/aos_warehouse/count";
 };
 
 export const exitPlanPath = () => {
-  return BASE_URL + "/api/v1/output_plan";
+  return getBaseUrl() + "/api/v1/output_plan";
 };
 export const filePath = () => {
-  return BASE_URL + "/api/v1/file";
+  return getBaseUrl() + "/api/v1/file";
 };
 
 export const packageShelfPath = () => {
-  return BASE_URL + "/api/v1/shelf_package";
+  return getBaseUrl() + "/api/v1/shelf_package";
 };
 
 export const appendixPath = () => {
-  return BASE_URL + "/api/v1/appendix";
+  return getBaseUrl() + "/api/v1/appendix";
 };
 
 export const shelfPath = () => {
-  return BASE_URL + "/api/v1/shelf";
+  return getBaseUrl() + "/api/v1/shelf";
 }
 
 export const countsPath = () => {
-  return BASE_URL + "/api/v1/counts";
+  return getBaseUrl() + "/api/v1/counts";
 };
 
 export const operationInstructionPath = () => {
-  return BASE_URL +  '/api/v1/operation_instruction'
+  return getBaseUrl() +  '/api/v1/operation_instruction'
 }
 
 export const linePath = (page?: number, rowsPerPage?: number) => {
@@ -185,15 +195,15 @@ export const linePath = (page?: number, rowsPerPage?: number) => {
   if (page && rowsPerPage) {
     params = `?current_page=${page}&number_of_rows=${rowsPerPage}`;
   }
-  return BASE_URL + "/api/v1/line_classification" + params;
+  return getBaseUrl() + "/api/v1/line_classification" + params;
 };
 
 export const countLinePath = () => {
-  return BASE_URL + "/api/v1/line_classification/count";
+  return getBaseUrl() + "/api/v1/line_classification/count";
 };
 
 export const removeLinePath = (id: number) => {
-  return `${BASE_URL}/api/v1/line_classification/${id}`;
+  return `${getBaseUrl()}/api/v1/line_classification/${id}`;
 };
 
 export const divisionPath = (page?: number, rowsPerPage?: number) => {
@@ -201,13 +211,13 @@ export const divisionPath = (page?: number, rowsPerPage?: number) => {
   if (page && rowsPerPage) {
     params = `?current_page=${page}&number_of_rows=${rowsPerPage}`;
   }
-  return BASE_URL + "/api/v1/regional_division" + params;
+  return getBaseUrl() + "/api/v1/regional_division" + params;
 };
 
 export const countDivisionPath = () => {
-  return BASE_URL + "/api/v1/regional_division/count";
+  return getBaseUrl() + "/api/v1/regional_division/count";
 };
 
 export const removeDivisionPath = (id: number) => {
-  return `${BASE_URL}/api/v1/regional_division/${id}`;
+  return `${getBaseUrl()}/api/v1/regional_division/${id}`;
 };
