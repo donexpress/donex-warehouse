239786a1c935a83ee74c266aa44f1657f41ca034
Yaniel Antonio Sánchez Domínguez <yanielsanchez930316@gmail.com>
2023-11-03 01:46:40 -0400

    UPDATE: updating

--- src/app/components/common/ManifestTableDialog.tsx
+++ src/app/components/common/ManifestTableDialog.tsx
@@ -66,6 +66,7 @@ const ManifestTableDialog = ({ close, content }: Params) => {
     }, [intl]);
 
     const getColumns = React.useMemo(() => {
+        
         const columns = [
             { name: "ID", uid: "id", sortable: true },
             { name: intl.formatMessage({ id: "waybill_id" }), uid: "waybill_id", sortable: true },
