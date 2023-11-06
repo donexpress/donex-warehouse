e4af7fa752f2257bd2cb14c29901878ec60c0758
Yaniel Antonio Sánchez Domínguez <yanielsanchez930316@gmail.com>
2023-11-06 13:46:49 -0500

    UPDATE: updating CopyColumnToClipboard with info message

--- src/app/components/common/CopyColumnToClipboard.tsx
+++ src/app/components/common/CopyColumnToClipboard.tsx
@@ -1,15 +1,21 @@
+import { showMsg } from "@/helperserege1992";
 import { FaCopy } from "react-icons/fa"
+import { useIntl } from "react-intl";
 
 interface Props {
     value: any
 }
 const CopyColumnToClipboard = ({ value }: Props) => {
+    const intl = useIntl();
+
     const handleCopy = () => {
-        if(typeof value === "string"){
-            navigator.clipboard.writeText(value.toString())            
+        if (typeof value === "string") {
+            navigator.clipboard.writeText(value.toString());
         } else {
             navigator.clipboard.writeText(value.props.children)
         }
+        const message = intl.formatMessage({ id: "copied_item" });
+        showMsg(message, { type: "success" });
     }
     return (
         <div className="copy-container">
