bc78eb090ce9d41191dcc1ea6c3daad046ad7461
Yaniel Antonio Sánchez Domínguez <yanielsanchez930316@gmail.com>
2023-12-13 17:52:53 -0500

    CREATE: creating summary logic and menu

---
+++ src/types/summary.ts
@@ -0,0 +1,11 @@
+
+export type Summary = {
+    id: number;
+    MWB: string;
+    quantity_package: number;
+    quantity_kilograms: number;
+    created_at: string;
+    quantity_shipping_cost: number;
+    quantity_sale_price: number;
+    earnings: number;
+}
